33a710b1749a015aeab78e0ff807ff2c9b4d75ad
lucapizzoni <luca.pizzoni@outlook.de>
2021-12-03 00:02:35 +0900

    feat: add date picker for add event activity

--- app/src/main/java/com/example/travelbucket/AddEventActivity.kt
+++ app/src/main/java/com/example/travelbucket/AddEventActivity.kt
@@ -1,30 +1,70 @@
 package com.example.travelbucket
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.core.widget.doOnTextChanged
+import android.view.View
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.TextView
 import com.example.travelbucket.databinding.ActivityAddEventBinding
-import com.example.travelbucket.databinding.ActivityMainBinding
 import com.google.android.material.textfield.TextInputEditText
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddEventActivity : AppCompatActivity() {
     val binding by lazy { ActivityAddEventBinding.inflate(layoutInflater) }
+
+    var btnDate: Button? = null
+    var textDate: TextView? = null
+    var cal = Calendar.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         var editTitle = findViewById<TextInputEditText>(R.id.textInputEditTextTitle)
         var editCosts = findViewById<TextInputEditText>(R.id.textInputEditTextCosts)
-        var editDate = findViewById<TextInputEditText>(R.id.textInputEditTextDate)
         var editLocation = findViewById<TextInputEditText>(R.id.textInputEditTextLocation)
         var editNotes = findViewById<TextInputEditText>(R.id.textInputEditTextNotes)
         var editLinks = findViewById<TextInputEditText>(R.id.textInputEditTextLinks)
         var editDuration = findViewById<TextInputEditText>(R.id.textInputEditTextDuration)
+
+        // get the references from layout file
+        textDate = binding.textDate
+        btnDate = binding.btnDate
+
+        textDate!!.text = "--/--/----"
+
+        // create an OnDateSetListener
+        val dateSetListener = object : DatePickerDialog.OnDateSetListener {
+            override fun onDateSet(
+                view: DatePicker, year: Int, monthOfYear: Int,
+                dayOfMonth: Int
+            ) {
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, monthOfYear)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                updateDateInView()
+            }
+        }
+
+        // when you click on the button, show DatePickerDialog that is set with OnDateSetListener
+        btnDate!!.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View) {
+                DatePickerDialog(
+                    this@AddEventActivity,
+                    dateSetListener,
+                    // set DatePickerDialog to point to today's date when it loads up
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+
+        })
+
 
 
         binding.btnCancelEvent.setOnClickListener {
@@ -37,12 +77,12 @@ class AddEventActivity : AppCompatActivity() {
             if (binding.textInputEditTextTitle.text!!.isEmpty()) {
                 binding.textInputLayoutTitle.error = "Title required!"
             } else {
+                var date = textDate
+                Log.d("ITM", "Title: $date")
                 var title = (editTitle.text).toString()
                 Log.d("ITM", "Title: $title")
                 var costs = (editCosts.text).toString()
                 Log.d("ITM", "costs: $costs")
-                var date = (editDate.text).toString()
-                Log.d("ITM", "date: $date")
                 var location = (editLocation.text).toString()
                 Log.d("ITM", "location: $location")
                 var notes = (editNotes.text).toString()
@@ -64,5 +104,10 @@ class AddEventActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         }
+    }
+    private fun updateDateInView() {
+        val myFormat = "MM/dd/yyyy" // mention the format you need
+        val sdf = SimpleDateFormat(myFormat, Locale.US)
+        textDate!!.text = sdf.format(cal.getTime())
     }
 }
